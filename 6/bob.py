a9bef867c9ef2394ea9aa4c54ff8244cc7e8d0cc
Hyunwoo Lee <hw5773@gmail.com>
2023-04-06 05:35:25 +0900

    update the code

---
+++ 6/bob.py
@@ -0,0 +1,58 @@
+import socket
+import threading
+import argparse
+import logging
+import random
+from etc import generate_messages
+
+def decrypt(key, iv, encrypted):
+    decrypted = encrypted
+    return decrypted
+
+def handler(alice, key, iv):
+    challenges = generate_messages()
+    rand = int(random.random() * len(challenges))
+    challenge = challenges[rand]
+    alice.send(challenge.encode())
+    logging.info("[*] Challenge: {}".format(challenge))
+    encrypted = alice.recv(1024).decode()
+    logging.info("[*] Received: {}".format(encrypted))
+    decrypted = decrypt(key, iv, encrypted)
+    logging.info("[*] Plaintext: {}".format(decrypted))
+
+    alice.close()
+
+def run(addr, port, key, iv):
+    bob = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    bob.bind((addr, port))
+
+    bob.listen(2)
+    logging.info("[*] Bob is Listening on {}:{}".format(addr, port))
+
+    while True:
+        alice, info = bob.accept()
+
+        logging.info("[*] Server accept the connection from {}:{}".format(info[0], info[1]))
+
+        handle = threading.Thread(target=handler, args=(alice, key, iv))
+        handle.start()
+
+def command_line_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--addr", metavar="<bob's IP address>", help="Bob's IP address", type=str, default="0.0.0.0")
+    parser.add_argument("-p", "--port", metavar="<bob's open port>", help="Bob's port", type=int, required=True)
+    parser.add_argument("-k", "--key", metavar="<shared key>", help="shared key", type=str, required=True)
+    parser.add_argument("-i", "--iv", metavar="<initialization vector>", help="initialization vector", type=str, required=True)
+    parser.add_argument("-l", "--log", metavar="<log level (DEBUG/INFO/WARNING/ERROR/CRITICAL)>", help="Log level (DEBUG/INFO/WARNING/ERROR/CRITICAL)", type=str, default="INFO")
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = command_line_args()
+    log_level = args.log
+    logging.basicConfig(level=log_level)
+
+    run(args.addr, args.port, args.key, args.iv)
+
+if __name__ == "__main__":
+    main()
